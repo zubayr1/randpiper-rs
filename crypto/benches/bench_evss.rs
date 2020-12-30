d63af99c5c83a887ec1c8358dd74181e1c0e13dc
zhtluo <zhtluo@gmail.com>
2020-12-30 16:03:17 -0500

    move setup out of sh_gen

--- crypto/benches/bench_evss.rs
+++ crypto/benches/bench_evss.rs
@@ -11,13 +11,13 @@ pub fn sh_gen(c: &mut Criterion) {
     let mut group = c.benchmark_group("sh_gen");
     let secret = F381::rand(rng);
     for n in 3..MAX_COUNT {
+        let t = (n + 1) / 2;
+        let degree = t - 1;
+        let params = EVSS381::setup(secret, degree, rng).expect("");
+        let mut shares = Vec::new();
         group.throughput(Throughput::Bytes(n as u64));
         group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
             b.iter(|| {
-                let t = (n + 1) / 2;
-                let degree = t - 1;
-                let params = EVSS381::setup(secret, degree, rng).expect("");
-                let mut shares = Vec::new();
                 for i in 0..n {
                     shares.push(EVSS381::get_share(F381::from((i + 1) as u32), &params, rng).expect(""));
                 }
