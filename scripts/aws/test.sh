8dc1c8c004f228811e8bf700caecd3bef3d0dabd
zhtluo <zhtluo@gmail.com>
2021-01-18 16:30:09 -0500

    added n3

--- scripts/aws/test.sh
+++ scripts/aws/test.sh
@@ -1,2 +1,2 @@
 killall -9 node-bft
-timeout 60 ./randpiper-rs/target/release/node-bft -c ./randpiper-rs/test/d100-n10/nodes-$1.json -d 500 -i ./randpiper-rs/ips_file > output.log
+timeout 600 ./randpiper-rs/target/release/node-bft -c ./randpiper-rs/test/d100-n10/nodes-$1.json -d 150 -i ./randpiper-rs/ips_file > output.log
