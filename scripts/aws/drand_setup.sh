e5f48e8b8e5ef0b270eea96e1a460648f1d56f75
hermitsage <dth.bht@gmail.com>
2021-01-18 01:08:15 -0500

    Add AWS scripts

---
+++ scripts/aws/drand_setup.sh
@@ -0,0 +1,11 @@
+# Script that must run on AWS
+
+# Update packages
+sudo pacman -Syu --noconfirm
+
+# Install go
+sudo pacman -S go git --noconfirm
+
+git clone https://github.com/drand/drand
+cd drand
+make install
