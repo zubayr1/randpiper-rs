b0144b13a8e82c58cb9b4ce80c391b3c41385e7f
zhtluo <zhtluo@gmail.com>
2021-01-19 12:04:45 -0500

    sleep time

--- scripts/aws/test.sh
+++ scripts/aws/test.sh
@@ -1,2 +1,2 @@
 killall -9 node-bft
-timeout 60 ./randpiper-rs/target/release/node-bft -c ./randpiper-rs/test/d100-n16/nodes-$1.dat -d 80 -i ./randpiper-rs/ips_file > output.log
+timeout 180 ./randpiper-rs/target/release/node-bft -c ./randpiper-rs/test/d100-n16/nodes-$1.dat -d 50 -i ./randpiper-rs/ips_file > output.log
